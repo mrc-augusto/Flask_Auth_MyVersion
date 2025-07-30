6ab074323c18711e450a014bdc2d69c98814d053
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-07-30 11:56:51 -0300

    User data created

---
+++ models/users.py
@@ -0,0 +1,10 @@
+from database import db
+from flask_login import UserMixin 
+
+class User(UserMixin, db.Model):
+  #id, username, password, email
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  password = db.Column(db.String(80), nullable=False)
+  email = db.Column(db.String(120), nullable=False, unique=True)
+  role = db.Column(db.String(20), nullable=False, default='user')
