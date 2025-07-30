2706d03e53781079ee06957073ebb8a492db18de
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-07-30 16:21:39 -0300

    CREATE, UPDATE & READ methods created

--- app.py
+++ app.py
@@ -41,6 +41,49 @@ def logout():
   logout_user()
   return jsonify({'message': 'Logout realizado com sucesso!'})
 
+@app.route('/user', methods=['POST'])
+@login_required
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+  email = data.get('email')
 
+  if username and password:
+    new_user = User(username=username, password=password, email=email, role='user')
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário criado com sucesso!'})
+  return jsonify({'message': 'Dados inválidos'}), 400
+
+@app.route('/user/<int:id>', methods=['GET'])
+@login_required
+def get_user(id):
+  user_id = User.query.get(id)
+  if user_id:
+    return jsonify({
+      'username': user_id.username, 
+      'email': user_id.email, 
+      'role': user_id.role
+    })
+  return jsonify({'message': 'Usuário não encontrado'}), 404
+
+@app.route('/user/<int:id>', methods=['PUT'])
+@login_required
+def update_user(id):
+  data = request.json
+  user_id = User.query.get(id)
+
+  if user_id != current_user.id and current_user.role != 'admin':
+    return jsonify({'message': 'Você não tem permissão para atualizar este usuário'}), 403
+  
+  if user_id:
+    user_id.password = data.get('password')
+    user_id.email = data.get('email')
+    user_id.role = data.get('role')
+    db.session.commit()
+    return jsonify({'message': f'Atualização do usuário {user_id.username} foi realizado com sucesso!'})
+  return jsonify({'message': 'Usuário não encontrado'}), 404
+  
 if __name__ == '__main__':
   app.run(debug=True)
